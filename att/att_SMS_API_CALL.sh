81a5345ba476be66435c9936a9d2e7cb8447851c
TimothyLe <timothyle85@gmail.com>
2018-03-24 18:09:57 -0700

    Gear

--- att/att_SMS_API_CALL.sh
+++ att/att_SMS_API_CALL.sh
@@ -2,7 +2,7 @@
 
 # Authentication parameter. For directions on how to obtain the OAuth access
 # token, see the OAuth section.
-OAUTH_ACCESS_TOKEN="BF-ACSI~4~20180324175500~PaZ852ugptTwfiqlLnBiQ5c0TMWz1zpy"
+OAUTH_ACCESS_TOKEN="BF-ACSI~4~20180325001822~ZCleLfIQn7OODNlq3WgxZSQ1icLkSn45"
 
 # Step 2:
 
@@ -14,18 +14,18 @@ SMS_MSG_TEXT="SMS Message Text"
 
 # Enter telephone number to which the SMS message will be sent.
 # For example: TEL="tel:+1234567890"
-TEL="+14089668385"
+TEL="+5716650861"
 
 # Send the Send SMS method request to the API Gateway.
-# curl "${FQDN}/sms/v3/messaging/outbox" \
-#     --header "Content-Type: application/json" \
-#     --header "Accept: application/json" \
-#     --header "Authorization: Bearer ${OAUTH_ACCESS_TOKEN}" \
-#     --data "{\"outboundSMSRequest\":{\"address\":\"${TEL}\",\"message\":\"${SMS_MSG_TEXT}\"}}" \
-#     --request POST
+curl "${FQDN}/sms/v3/messaging/outbox" \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/json" \
+    --header "Authorization: Bearer ${OAUTH_ACCESS_TOKEN}" \
+    --data "{\"outboundSMSRequest\":{\"address\":\"${TEL}\",\"message\":\"${SMS_MSG_TEXT}\"}}" \
+    --request POST
 
-curl -k  -H "Accept: application/json" \ 
-		 -H "Content-type: application/json" \
-		 -H "Authorization: Bearer ${OAUTH_ACCESS_TOKEN}" \
-		 -X POST ${FQDN}/sms/v3/messaging/outbox \
-		 -d {"outboundSMSRequest": {"address":"tel:${TEL}","message":"${SMS_MSG_TEXT}"}}
+# curl -k  -H "Accept: application/json" \ 
+# 		 -H "Content-type: application/json" \
+# 		 -H "Authorization: Bearer ${OAUTH_ACCESS_TOKEN}" \
+# 		 -X POST ${FQDN}/sms/v3/messaging/outbox \
+# 		 -d {"outboundSMSRequest": {"address":"tel:${TEL}","message":"${SMS_MSG_TEXT}"}}
--- att/att_sms_access_token.sh
+++ att/att_sms_access_token.sh
@@ -1,6 +1,6 @@
 # Obtain these from your account on the AT&T Developer Program website.
-APP_KEY="ntswdqivagmrr5ectvwrzpx9tev2emhg"
-APP_SECRET="xoa4fjxvijx2z3kuuvc0cary1hzvtdkq"
+APP_KEY="kmojirew4nleokibe3tjx8h5pokk2ldt"
+APP_SECRET="xqud3ctiophupefoz3uytay00a8isaik"
 
 # Set up the scopes for requesting API access. 
 API_SCOPES="SMS"
@@ -10,3 +10,5 @@ curl "https://api.att.com/oauth/v4/token" \
     --header "Accept: application/json" \
     --header "Content-Type: application/x-www-form-urlencoded" \
     --data "client_id=${APP_KEY}&client_secret=${APP_SECRET}&grant_type=client_credentials&scope=${API_SCOPES}"
+
+# curl https://api.att.com/oauth/v4/token --request POST --insecure --data "client_id=kmojirew4nleokibe3tjx8h5pokk2ldt&client_secret=xqud3ctiophupefoz3uytay00a8isaik&grant_type=client_credentials&scope=SMS"
